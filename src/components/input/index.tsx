8ef9958024857bd35347a3fe1bbcf1a657cbdc20
David Pachinik <ing.pachinik96@gmail.com>
2026-01-15 17:22:36 -0300

    feat:componente Input reutilizable

---
+++ src/components/input/index.tsx
@@ -0,0 +1,27 @@
+ import type {RegisterOptions, UseFormRegister} from 'react-hook-form'
+
+interface InputProps{
+    type:string;
+    name:string;
+    placeholder:string;
+    register:UseFormRegister<any>;
+    error?:string;
+    rules?:RegisterOptions;
+}
+
+
+export function Input({type, name, placeholder, register,rules, error}:InputProps){
+    return(
+        <div className='w-full mt-2'>
+            <input
+                className='w-full max-w-2xl pl-2 rounded-sm h-8 border border-[#3D4035] outline-none' 
+                type={type}
+                placeholder={placeholder}
+                {...register(name,rules)}
+                id='name'
+            />
+            {error && <p>{error}</p>}
+        </div>
+
+    )
+}
