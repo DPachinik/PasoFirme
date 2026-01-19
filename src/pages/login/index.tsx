e445b6f0a3ac5a81cd0d8c18a377f24d432f53b0
David Pachinik <ing.pachinik96@gmail.com>
2026-01-19 15:56:03 -0300

    fix: prevenir renderizado del login con sesión activa

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import { Input } from "../../components/input";
 import {useForm} from 'react-hook-form'
 import {  z } from 'zod'
@@ -7,6 +7,8 @@ import { auth } from '../../services/firebaseConnection'
 import { signInWithEmailAndPassword } from 'firebase/auth'
 import { useNavigate } from 'react-router-dom'
 import  Toast  from 'react-hot-toast';
+import { useContext } from 'react';
+import { AuthContext} from '../../contexts/auth/authContext'
 
 
 
@@ -21,17 +23,20 @@ const schema = z.object({
 
 type FormData = z.infer< typeof schema>
 
+
+
 export function Login(){
 
+    const { loadingAdmin, signed } = useContext(AuthContext);
+  
     const navigate = useNavigate();
-
-
-
     const { register, handleSubmit, formState:{errors}} = useForm<FormData>({
         resolver:zodResolver(schema),
         mode:"onChange"
         
     })
+
+
 
     function onSubmit(data:FormData){
         signInWithEmailAndPassword(auth, data.email, data.password)
@@ -71,6 +76,15 @@ export function Login(){
         });
 
 
+    }
+
+    if(loadingAdmin){
+        return <div className='h-screen w-full items-center justify-center text-2xl font-semibold'>Cargando...</div>
+    }
+
+    if(signed){
+        
+        return <Navigate to='/dashboard' replace />
     }
 
 
