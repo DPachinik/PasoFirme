29c42f9cccaa755d01a90305f35a6722e5fc8ffa
David Pachinik <ing.pachinik96@gmail.com>
2026-02-06 17:11:45 -0300

    feat(home): listar productos desde Firebase y mejorar UX evitando layout shift de imágenes

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,45 +1,75 @@
 import { useState, useEffect, useContext } from 'react'
 import { CartContext } from '../../contexts/cart/CartContext'
-import { useNavigate } from 'react-router-dom'
-
-import { BsCartPlus } from "react-icons/bs";
-import {api} from '../../services/api'
+import { Link } from 'react-router-dom'
 import  Toast  from 'react-hot-toast';
 import { Header } from '../../components/header';
-
-
+import {getDocs, collection, query, orderBy} from 'firebase/firestore'
+import {db} from '../../services/firebaseConnection'
 
 
 export interface ProductsProps{
-    id:number;
-    title:string;
-    description:string;
-    price:number;
-    cover:string;
+    id:string;
+    modelo:string;
+    calceMin:string;
+    calceMax?:string;
+    precio:number;
+    color:string;
+    estado:string;
+    descripcion:string;
+    imagenes:ImageProps[];
 }
 
+type ImageProps ={
+    idImage:string;
+    uid:string;
+    url:string;
+}
 
 
 export function Home(){
 
     const {addItemCart} = useContext(CartContext)
-    const  navigate  = useNavigate()
- 
 
     const[products, setProducts] = useState<ProductsProps[]>([]);
+    const[loadImage, setLoadImage] = useState<string[]>([]);
+
 
     useEffect(()=>{
         async function getProducts(){
-            const response = await api.get("/products")
-            setProducts(response.data)
+            
+            const productsRef = collection(db, 'shoes');
+            const queryRef = query(productsRef, orderBy('created', 'desc'))
+
+            getDocs(queryRef)
+            .then((snapshot)=>{
+                const list = [] as ProductsProps[];
+
+                snapshot.forEach((product)=>{
+                    list.push({
+                        id:product.id,
+                        modelo:product.data().modelo,
+                        calceMin:product.data().calceMin,
+                        calceMax:product.data().calceMax,
+                        precio:Number(product.data().precio),
+                        color:product.data().color,
+                        estado:product.data().estado,
+                        descripcion:product.data(). descripcion,
+                        imagenes:product.data().imagenes,
+
+                    })
+                })
+
+                setProducts(list)
+
+            })
+            .catch((error)=>{
+                console.log('error:' + error);
+            })
         }
 
         getProducts();
     },[])
 
-    function handleDetail(id:number){
-        navigate(`/cart/${id}`)
-    }
 
 
     function handleAddItemCart(product:ProductsProps){
@@ -47,6 +77,10 @@ export function Home(){
         Toast.success('Producto añadido al carrito',{
             style:{background:'#121212',color:'#ffff'}
         })
+    }
+
+    function handleImageLoad(id:string){
+        setLoadImage((prevImage)=>[...prevImage, id])
     }
 
     return(
@@ -57,38 +91,76 @@ export function Home(){
                 
                 <h1 className="font-bold text-2xl mt-10 mb-4 text-center text-primary">Productos Destacados</h1>
 
-                <div className="grid grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-4 ">
+                <div className=" grid grid-cols-1  md:grid-cols-3 lg:grid-cols-4 gap-2 place-items-center">
                     {products.map((product)=>(
                         <section 
                         key={product.id}
-                        className="w-full  bg-white rounded-lg px-2 flex flex-col ">
-                            <img 
-                            className="w-full rounded-lg max-h-60 mb-2"
-                            src={product.cover} alt="producto"
-                        
-                            />
-                            <div className='w-full flex justify-end'>
-                                <span
-                                onClick={()=>handleDetail(product.id)}  
-                                className='bg-black w-fit text-white text-sm px-2 rounded-xl mb-2  hover:cursor-pointer transition-all'>ver detalle</span>
+                        className="relative w-full max-w-xs lg:max-w-[90%] h-[380px] bg-amber-300 rounded-lg  flex flex-col items-center">
+
+
+                            <div 
+                            className='w-full min-h-[200px] bg-slate-100 rounded-t-lg'
+                            style={{display:loadImage.includes(product.id)? 'none':'block'}}
+                            >
+
                             </div>
+                            <div className='w-full min-h-[200px]  rounded-t-lg  '>
+                                <Link to={`/detail/${product.id}`}>
+                                    <img 
+                                    className="w-full h-full rounded-t-lg bg-primary mb-2 object-cover "
+                                    src={product.imagenes[0].url} alt="producto"
+                                    onLoad={()=>handleImageLoad(product.id)} 
+                                    style={{display:loadImage.includes(product.id)? 'block':'none'}}                   
+                                    />
+
+                                </Link>
+                            </div>
+
+                            <div className='absolute top-[180px] h-[200px] w-full flex flex-col   px-3 rounded-xl border bg-amber-50'>
+                                
+                                <div className='flex flex-col'>
+                                    <p className="font-bold mt-2 mb-2 text-black/80 uppercase">{product.modelo}</p>
+                                    <div className='flex flex-row items-start gap-2 text-gray-500 font-medium '>
+                                        <div className="border border-gray-500 w-fit p-0.5 px-1 text-[10px] font-bold  rounded-sm ">
+                                            {product.calceMax
+                                                ? `BRA ${product.calceMin} | ${product.calceMax}`
+                                                :`BRA ${product.calceMin}`}
+                                        </div>
+
+
+                                        <div className='flex  gap-2 border w-fit  p-0.5 px-1 text-[10px] font-bold rounded-sm uppercase items-center'>
+                                            <span >Color</span>
+                                            <span className='w-3 h-3  rounded-full' style={{backgroundColor:product.color}} />
+                                        </div>
+
+
+                                    </div>
+                                </div>
+
+                                <div className='text-sm text-gray-600  mt-3'>{product.descripcion}</div>
+
+                                <div className="flex h-full items-end justify-between  mb-4 ">
+                                    <div className='flex flex-col'>
+                                        <span className='text-[11px] font-bold text-gray-600'>PRECIO</span>
+                                        <strong className="text-secondary text-lg">
+                                            {product.precio.toLocaleString("es-PY",{
+                                                style:'currency',
+                                                currency:'PYG'
+                                            })}
+                                        </strong>
+                                    </div>
+
+
+                                    <button onClick={()=>handleAddItemCart(product)} className="bg-primary p-1 px-2 rounded cursor-pointer text-sm font-medium text-white">
+                                        añadir al carrito
+                                    </button>
+                                </div>
+
+                            </div>
+
+                           
                             
-                            <div className='h-px w-full max-w-90 bg-primary '></div>
-                            <p className="font-medium mt-1 mb-2 text-primary">{product.title}</p>
-
-                            <div className="flex h-full items-end justify-between gap-3 mb-2  ">
-                                <strong className="text-secondary">
-                                    {product.price.toLocaleString("es-PY",{
-                                        style:'currency',
-                                        currency:'PYG'
-                                    })}
-                                </strong>
-
-                                <button onClick={()=>handleAddItemCart(product)} className="bg-primary p-1 rounded cursor-pointer">
-                                    <BsCartPlus size={20} color="#ffff"/>
-                                </button>
-                            </div>
-                    </section>
+                        </section>
                     ))}
                 </div>
             </main>
