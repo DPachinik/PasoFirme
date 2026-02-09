3ca2eb2e41a3414cef1e64f5da3d49d0fe7a4cd8
David Pachinik <ing.pachinik96@gmail.com>
2026-02-09 15:50:56 -0300

    feat: implementar página de detalles con búsqueda por ID de producto y función de agregar al carrito

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,12 +1,33 @@
 import {useEffect, useState} from 'react';
 import {useParams} from 'react-router-dom';
-import { api } from '../../services/api';
-import type { ProductsProps } from '../home';
 import {useContext} from 'react'
 import { CartContext } from '../../contexts/cart/CartContext';
 import toast from 'react-hot-toast';
-import { BsCartPlus } from 'react-icons/bs';
 import { useNavigate } from 'react-router-dom'
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../../services/firebaseConnection';
+import { FaCheck } from 'react-icons/fa';
+
+
+export interface ProductsProps{
+    id:string;
+    modelo:string;
+    calceMin:string;
+    calceMax?:string;
+    precio:number;
+    color:string;
+    estado:string;
+    descripcion:string;
+    imagenes:ImageProps[];
+}
+
+type ImageProps ={
+    idImage:string;
+    uid:string;
+    url:string;
+}
+
+
 
 
 export function Detail(){
@@ -17,10 +38,26 @@ export function Detail(){
     const {addItemCart}=useContext(CartContext)
 
     useEffect(()=>{
-        async function productDetail(){  
-                const response = await api.get(`products/${id}`)
-                setProduct(response.data);
-                console.log(response.data)
+        async function productDetail(){ 
+            if(!id){
+                return;
+            } 
+
+            const productRef = doc(db, 'shoes', id);
+             getDoc(productRef)
+             .then((snapshot)=>{
+                setProduct({
+                    id:snapshot.id,
+                    modelo:snapshot.data()?.modelo,
+                    calceMin:snapshot.data()?.calceMin,
+                    calceMax:snapshot.data()?.calceMax,
+                    precio:Number(snapshot.data()?.precio),
+                    color:snapshot.data()?.color,
+                    estado:snapshot.data()?.estado,
+                    descripcion:snapshot.data()?.descripcion,
+                    imagenes:snapshot.data()?.imagenes
+                })
+             }) 
 
         };
 
@@ -37,30 +74,91 @@ export function Detail(){
         navigate('/cart')
     }
 
+    if(!product){
+        
+       return <h2>Cargando Producto</h2>
+    }
+
     return(
-        <main  className="w-full max-w-7xl px-4 mx-auto ">
-            <section className='grid grid-cols-2 mt-8 place-items-center' >
-                <img
-                src={product?.cover}
-                alt={product?.title}
-                className='w-full max-w-9/12 h-90 object-fill object-center'
-                />
 
-                <div className='flex flex-col gap-4 py-4'>
+        
+        <main  className=" w-full max-w-7xl px-4 mx-auto flex items-center justify-center ">
+            <section className='grid grid-cols-2 mt-8 w-full place-items-center' >
+                <div className='flex  flex-col gap-4'>
+                    <div className='grid grid-cols-2 gap-4 max-w-lg 0 mx-auto'>
+                        {product?.imagenes.map((imagen)=>(
+                            <div key={imagen.idImage} className='flex h-38 w-48 items-center justify-center rounded-lg'>
+                                <img 
+                                src={imagen.url} 
+                                alt='calzado de cuero'
+                                className='w-full h-full rounded-lg object-cover hover:scale-125 transition-all'
+                                />
+                            </div>
+                        ))}
+                    </div>
 
-                    <h1 className='text-xl font-medium '>{product?.title}</h1>
-                    <p>{product?.description}</p>
-                    <div className='flex items-center justify-between w-fit gap-6 px-2'>
-                        <strong>Precio: {product?.price.toLocaleString('es-PY',{
+                    <div className=' pb-2 pl-8 flex flex-col gap-2 mt-2'>
+                        <h2>Características  </h2>
+                        <ul className='text-sm'>
+                            <li className='flex items-center gap-2'><FaCheck color="green" /> Materiales duraderos pensados para acompañarte por mucho tiempo</li>
+                            <li className='flex items-center gap-2'><FaCheck color="green" /> Estilo moderno y funcional, ideal para cualquier ocasión</li>
+                            <li className='flex items-center gap-2'><FaCheck color="green" /> Sensación de comodidad continua gracias a su diseño ergonómico</li>
+                            <li className='flex items-center gap-2'><FaCheck color="green" /> Modelos originales que marcan la diferencia</li>
+                            <li className='flex items-center gap-2'><FaCheck color="green" /> Fácil de cuidar y diseñado para resistir el uso diario</li>
+
+                        </ul>
+                    </div>
+                </div>
+               
+
+               
+
+                <div className='h-full mb-4 flex flex-col  p-4 bg-amber-50  justify-evenly w-full max-w-[75%] rounded-lg shadow '>
+
+                    <h1 className='uppercase text-2xl font-bold'> {product.modelo}</h1>
+
+                    <div className='flex text-xl'>
+                        <strong>{product?.precio.toLocaleString('es-PY',{
                             style:'currency',
                             currency:'PYG'
                         })}</strong>
-                    <button onClick={() => product && handleAddItemCart(product)} className="bg-zinc-900 p-1 rounded">
-                        <BsCartPlus size={20} color="#ffff"/>
-                    </button>
+                    
+
                     </div>
 
+                    <div className='flex flex-col gap-4 '>
+
+                        <div className='flex flex-col '>
+                            <span>TAMAÑOS DISPONIBLES:</span>
+                            <span>{product.calceMin} - {product.calceMax} </span>
+                        </div>
+                            
+
+                        <div className='flex items-center gap-4'>
+                            <span>COLOR:</span>
+                            <span className='h-4 w-4 rounded-full' style={{backgroundColor:product.color}}></span>
+                        </div>
+
+                    </div>
+
+                            
+
+                    <div className='flex flex-col '>
+                        <span>DESCRIPCIÓN DEL CALZADO:</span>
+                        <span>{product.descripcion}</span>
+                    </div>
+                            
+                    <div className='w-full  flex items-center justify-center '>
+                        <button onClick={() => product && handleAddItemCart(product)} className="bg-secondary p-1 rounded  w-full max-w-sm flex items-center justify-center gap-4 px-2 cursor-pointer">
+                            añadir al carrito
+                        </button>
+                    </div>
+                        
+                            
+
                 </div>
+                    
+    
 
 
             </section>
