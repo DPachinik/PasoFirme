a4087474e897dc30ca5bc1934cb339bb529760aa
David Pachinik <ing.pachinik96@gmail.com>
2026-02-23 19:16:16 -0300

    refactor(home): eliminar consultas directas y usar lógica centralizada del contexto, agregar paginación con carga progresiva

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,8 @@
 import { useState, useEffect, useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { Header } from '../../components/header';
-import {getDocs, collection, query, orderBy} from 'firebase/firestore'
-import {db} from '../../services/firebaseConnection'
 import { ProductsContext } from '../../contexts/products/ProductsContext';
+
 
 
 export interface ProductsProps{
@@ -28,61 +27,30 @@ export interface ProductsProps{
 
 export function Home(){
 
-    const {products, setProducts}= useContext(ProductsContext)
+    const { products, getProducts, empty,setEmpty, isFiltered , setIsFiltered, setDoc, loadInitialProducts}= useContext(ProductsContext)
 
     const[loadImage, setLoadImage] = useState<string[]>([]);
     
+    
 
-      function getProducts(){
-            
-            const productsRef = collection(db, 'shoes');
-            const queryRef = query(productsRef, orderBy('created', 'desc'))
-
-            getDocs(queryRef)
-            .then((snapshot)=>{
-                const list = [] as ProductsProps[];
-
-                snapshot.forEach((product)=>{
-                    list.push({
-                        id:product.id,
-                        modelo:product.data().modelo,
-                        calceMin:product.data().calceMin,
-                        calceMax:product.data().calceMax,
-                        precio:Number(product.data().precio),
-                        color:product.data().color,
-                        estado:product.data().estado,
-                        descripcionCorta:product.data().descripcionCorta,
-                        descripcion:product.data(). descripcion,
-                        imagenes:product.data().imagenes,
-
-                    })
-                })
-
-                setProducts(list)
-
-           
-            })
-            .catch((error)=>{
-                console.log('error:' + error);
-            })
-        }
-
-          
 
     useEffect(()=>{
-
-        getProducts();
+        
+        loadInitialProducts();
     },[])
 
-
-
-
-
+    function TotalProducts(){
+        setDoc(null);
+        loadInitialProducts();
+        setIsFiltered(false)
+        setEmpty(false);
+    }
 
 
     function handleImageLoad(id:string){
         setLoadImage((prevImage)=>[...prevImage, id])
     }
+
 
     return(
         <div className='mx-2'>
@@ -95,17 +63,17 @@ export function Home(){
                 <div className='w-full h-10 flex items-center  rounded-lg  my-4 pl-4'>
                     <button 
                     className='border rounded-2xl  text-sm font-bold bg-black text-white py-1 px-6 cursor-pointer'
-                    onClick={getProducts}
+                    onClick={TotalProducts}
+                    style={isFiltered?{}:{display:'none'}}
                     >VER TODOS</button>
 
                 </div>
 
-                <div className=" grid grid-cols-1  md:grid-cols-3 lg:grid-cols-4 gap-2 place-items-center">
+                <div className=" grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 place-items-center">
                     {products.map((product)=>(
                         <section 
                         key={product.id}
                         className="relative w-full max-w-xs lg:max-w-[90%] h-[380px] bg-amber-300 rounded-lg  flex flex-col items-center">
-
 
                             <div 
                             className='w-full min-h-[200px] bg-slate-100 rounded-t-lg'
@@ -116,7 +84,7 @@ export function Home(){
                             <div className='w-full min-h-[200px]  rounded-t-lg  '>
                                 
                                     <img 
-                                    className="w-full h-full rounded-t-lg bg-primary mb-2 object-cover "
+                                    className="w-full h-full rounded-t-lg bg-primary mb-2 object-contain object-top p-4"
                                     src={product.imagenes[0].url} alt="producto"
                                     onLoad={()=>handleImageLoad(product.id)} 
                                     style={{display:loadImage.includes(product.id)? 'block':'none'}}                   
@@ -125,7 +93,7 @@ export function Home(){
                                 
                             </div>
 
-                            <div className='absolute top-[180px] h-[200px] w-full flex flex-col   px-3 rounded-xl border bg-amber-50'>
+                            <div className='absolute top-[180px] h-[200px] w-full flex flex-col   px-3 rounded-xl  bg-amber-50'>
                                 
                                 <div className='flex flex-col'>
                                     <p className="font-bold mt-2 mb-2 text-black/80 uppercase">{product.modelo}</p>
@@ -167,12 +135,18 @@ export function Home(){
                                     
                                 </div>
 
-                            </div>
-
-                           
-                            
+                            </div>                            
                         </section>
                     ))}
+                </div>
+                <div>
+                    <button
+                    onClick={getProducts}
+                    className='bg-black text-white px-4'
+                    style={empty || isFiltered?{display:'none'}:{} }
+                    >
+                        ver más
+                    </button>
                 </div>
             </main>
         </div>
