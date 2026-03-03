250a20ffc6d63c9782bf017ee690a89f77738857
David Pachinik <ing.pachinik96@gmail.com>
2026-03-03 15:22:14 -0300

    agregar componente de búsqueda para filtro de calzados

---
+++ src/components/InputSearch/InputSearch.tsx
@@ -0,0 +1,45 @@
+import { useState, useContext} from "react";
+import { IoSearch } from "react-icons/io5";
+import { ProductsContext } from "../../contexts/products/ProductsContext";
+
+
+export function InputSearch(){
+
+    const {searchProducts} =useContext(ProductsContext)
+    
+    const [input, setInput] =useState<string>('');
+
+
+    function handleSearch(){
+        
+    if(input===''){
+        return;
+    }
+        searchProducts(input);
+        setInput('');
+    }
+
+
+    return(
+
+        <div className='flex w-full items-center justify-center gap-2'>
+            <input
+                className='w-full max-w-2xl pl-2  h-8 border-b border-slate-400 outline-none text-white ' 
+                type='text' 
+                placeholder='Buscar'
+                value={input}
+                onChange={(e)=>setInput(e.target.value)}
+                onKeyDown={(e) => e.key === 'Enter' && handleSearch()}
+            />
+
+            <button 
+                className=''
+                onClick={handleSearch}
+            >
+                <IoSearch  size={28} className='cursor-pointer text-[#2A4D4E] hover:text-[#C14426]' />
+            </button>
+                           
+        </div>
+    )
+
+}
