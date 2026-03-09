159304d3761854fa3ae54722f633d2f21e8282dd
David Pachinik <ing.pachinik96@gmail.com>
2026-03-09 14:18:56 -0300

    style(components): actualizar colores de marca en el componente Input

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -12,13 +12,13 @@ interface InputProps{
 
 export function Input({type, name, placeholder, register,rules, error}:InputProps){
 
-    const baseStyles = 'w-full max-w-3xl text-xs  rounded-sm outline-none border border-[#3F4336]  h-7 pl-2 '
+    const baseStyles = 'w-full max-w-3xl text-xs  rounded-sm outline-none border border-[#2A4D4E]  h-7 pl-2 bg-white text-[#2A4D4E]'
 
     const typeStyles = 'h-6 w-10  rounded-full cursor-pointer border-none'
     return(
         <div className='w-full'>
             <input
-                className={` ${type === 'color'? typeStyles : baseStyles} ${error? 'border-red-500':'border-[#3D4035]'} `} 
+                className={` ${type === 'color'? typeStyles : baseStyles} ${error? 'border-red-500':'border-[#2A4D4E]'} `} 
                 type={type}
                 placeholder={ type !== 'color'? placeholder: undefined}
                 {...register(name,rules)}
